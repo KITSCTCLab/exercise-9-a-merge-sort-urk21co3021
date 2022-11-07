9f76ec310d6ae143d95eb2ce337adf69584978f8
urk21co3021 <110015333+urk21co3021@users.noreply.github.com>
2022-11-07 19:59:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,44 @@
+
 from typing import List
 
-def merge_sort(data) -> None:
+def merge(left_array: List[int], m: int, right_array: List[int], n: int) -> None:
   # Write code here
+  left_array= left_array[:m]
+  right_array= right_array[:n]
+  data=left_array + right_array
+  i=0
+  j=0
+  k=0
+
+  while i < len(left_array) and j < len(right_array):
+    if left_array[i] <= right_array[j]:
+      data[k] = left_array[i]
+      i += 1
+    else:
+      data[k] = right_array[j]
+      j += 1
+    k += 1
+
+  while i < len(left_array):
+    data[k] = left_array[i]
+    i += 1
+    k += 1
+
+  while j < len(right_array):
+     data[k]=right_array[j]
+     j += 1
+     k += 1
+  print(data)
+
 
 
 # Do not change the following code
-input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-merge_sort(data)
-print(data)
+nums1 = []
+nums2 = []
+for item in input().split(', '):
+  nums1.append(int(item))
+for item in input().split(', '):
+  nums2.append(int(item))
+m = int(input())
+n = int(input())
+merge(nums1, m, nums2, n)
